a8003e3dfd7e9d5a16125dc53ff7f36abd8a65e3
Vishal Garg <vishugarg10004@gmail.com>
2020-10-31 01:45:48 +0530

    [Vishal] added 3rd test case

--- IndianStateCensusAnalyserTest/UnitTest1.cs
+++ IndianStateCensusAnalyserTest/UnitTest1.cs
@@ -58,6 +58,18 @@ namespace IndianStateCensusAnalyserTest
             //total no of rows excluding header are 29 in indian state census data.
             Assert.AreEqual(CensusAnalyserException.ExceptionType.FILE_NOT_FOUND,customException.etype );
         }
-       
+        [Test]
+        /// <summary>
+        /// checking the program for incorrect file type which do not exist
+        /// </summary>
+        public void GivenWrongIndianCensusDataType_WhenReadedShouldReturnCustomException()
+        {
+            //census Analyser Class is Called and parameters are passed like country, indian state census data which is csv file and header file.
+            //add
+            var customException = Assert.Throws<CensusAnalyserException>(() => censusAnalyser.LoadCensusData(CensusAnalyser.Country.INDIA,indiaStateCensusDataText, indianStateCensusHeaders));
+            //total no of rows excluding header are 29 in indian state census data.
+            Assert.AreEqual(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE, customException.etype);
+        }
+
     }
 }
